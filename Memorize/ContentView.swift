2902b06e28363189aedacd1194097c8b76616a03
Fredy Sanchez <jalfredo.sanchezpt@gmail.com>
2023-03-07 22:41:39 -0600

    Add UI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,95 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🛴", "🚲", "🛵"].shuffled()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Memorize!")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+                    ForEach(emojis, id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .foregroundColor(.red)
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                carButton
+                Spacer()
+                sportsButton
+                Spacer()
+                animalsButton
+                Spacer()
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
-        .padding()
+        .padding(.horizontal)
+    }
+    
+    var carButton: some View {
+        Button {
+            emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🛴", "🚲", "🛵"].shuffled()
+        } label: {
+            VStack {
+                Image(systemName: "car")
+                Text("Vehicles")
+                    .font(.system(size: 14))
+            }
+        }
+    }
+    
+    var sportsButton: some View {
+        Button {
+            emojis = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🏐", "🏉", "🥏", "🎱", "🏓", "🏸", "🏑", "🏏"].shuffled()
+        } label: {
+            VStack {
+                Image(systemName: "figure.run")
+                Text("Sports")
+                    .font(.system(size: 14))
+            }
+        }
+    }
+    
+    var animalsButton: some View {
+        Button {
+            emojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️"].shuffled()
+        } label: {
+            VStack {
+                Image(systemName: "pawprint")
+                Text("Animals")
+                    .font(.system(size: 14))
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    // This variable is pointing to some location in memory outside of the
+    // allocated memory for this view, so whenever its state (value) changes,
+    // swiftui will rebuild this entire view
+    @State var isFaceUp = true
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let cardShape = RoundedRectangle(cornerRadius: 25.0)
+            if isFaceUp {
+                cardShape.foregroundColor(.white)
+                cardShape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                cardShape
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
